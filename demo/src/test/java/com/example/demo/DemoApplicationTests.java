d37109fc18557d1bf01fa5852b0295e3e7e4a53d
Athul86329 <2021wa86329@wilp.bits-pilani.ac.in>
2025-07-14 22:15:08 +0530

    source update Update DemoApplicationTests.java

--- demo/src/test/java/com/example/demo/DemoApplicationTests.java
+++ demo/src/test/java/com/example/demo/DemoApplicationTests.java
@@ -5,7 +5,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class DemoApplicationTests {
-
+System.out.println("Hello java ");
 	@Test
 	void contextLoads() {
 	}
