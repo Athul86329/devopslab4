b7666130c2528b9575ba447a1488c3c476c8dd1f
Athul86329 <2021wa86329@wilp.bits-pilani.ac.in>
2025-07-14 22:09:08 +0530

    Update DemoApplication.java

--- demo/src/main/java/com/example/demo/DemoApplication.java
+++ demo/src/main/java/com/example/demo/DemoApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class DemoApplication {
+	System.out.println("i created java program");
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
